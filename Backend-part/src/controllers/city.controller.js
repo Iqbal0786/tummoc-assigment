b6992e40beb16d2bb1da35b92917be41491c41e6
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-21 20:38:28 +0530

    added post route and populate data in get request

--- Backend-part/src/controllers/city.controller.js
+++ Backend-part/src/controllers/city.controller.js
@@ -3,13 +3,22 @@ const City= require("../models/city.model")
 const router = express.Router();
 
 router.get("",async(req,res)=>{
-    console.log(req.session);
+  
     try {
-        let cities= await City.find().lean().exec();
+        let cities= await City.find().populate({path:"user_id" , select:["name" ,'email']}).lean().exec();
         return res.status(200).send(cities);
     } catch (error) {
         return res.status(500).send(error.message)
     }
 })
 
+router.post("",async(req,res)=>{
+
+    try {
+        let city= await City.create(req.body);
+        return res.status(201).send(city);
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+})
 module.exports = router;
