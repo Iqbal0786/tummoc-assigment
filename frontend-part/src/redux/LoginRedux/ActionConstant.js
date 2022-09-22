db2d0d6e3781f7a9f51af79001ead0440ba00fd1
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-22 11:42:06 +0530

    created action constant for login redux

---
+++ frontend-part/src/redux/LoginRedux/ActionConstant.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export const LOADING="LOADING";
+export const ERROR="ERROR";
+export const SUCCESS="SUCCESS";
+
+export const loading=()=>({type:LOADING});
+export const error=()=>({type:ERROR});
+export const success=(payload)=>({type:SUCCESS ,payload})
+
+export const Register_Request=(data)=>(dispatch)=>{
+      dispatch(loading());
+      axios.post("http://localhost:9999/register" , data).then((res)=>{
+        console.log(res.data)
+        dispatch(success())
+      }).catch((err)=>{
+        console.log(err.message)
+        dispatch(error())
+      })
+  
+
+}
