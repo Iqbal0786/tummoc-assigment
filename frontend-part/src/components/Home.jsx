ec794d58579460435329b5dcc2daf0e8466f4052
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-22 15:21:41 +0530

    Update Home.jsx

--- frontend-part/src/components/Home.jsx
+++ frontend-part/src/components/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { useSelector } from 'react-redux'
  import "../styles/style.css"
 export default function Home() {
-  const {loading,error}= useSelector((store)=>store.loginReducer);
-  console.log(loading)
+  const { isLogged}= useSelector((store)=>store.loginReducer);
+  console.log(isLogged)
   return (
     <div className='home_main'>
       <h1>Welcome to Home page </h1>
