651848567b224af0c8976f0ad72aac2bbfd46f50
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-22 10:34:45 +0530

    added home and login route

--- frontend-part/src/allRoutes/AllRoutes.jsx
+++ frontend-part/src/allRoutes/AllRoutes.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../components/Home'
+import Login from '../components/Login'
 
 export default function AllRoutes() {
   return (
    <>
-   
+    <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+    </Routes>
    </>
   )
 }
