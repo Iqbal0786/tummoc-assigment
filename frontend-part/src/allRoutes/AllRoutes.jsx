71f961cb7a6114273365a395b5999ff716f87dc0
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-22 10:43:55 +0530

    updated routes and add css property

--- frontend-part/src/allRoutes/AllRoutes.jsx
+++ frontend-part/src/allRoutes/AllRoutes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../components/Home'
 import Login from '../components/Login'
+import Signup from '../components/Signup'
 
 export default function AllRoutes() {
   return (
@@ -9,6 +10,7 @@ export default function AllRoutes() {
     <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Signup/>}/>
     </Routes>
    </>
   )
