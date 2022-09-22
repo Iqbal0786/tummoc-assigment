28a9789ae53c97facfd65e3b05701c2e4f591e40
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-22 15:07:28 +0530

    Update store.js

--- frontend-part/src/redux/store.js
+++ frontend-part/src/redux/store.js
@@ -1,8 +1,11 @@
 import {createStore,combineReducers , applyMiddleware} from "redux";
 import thunk from "redux-thunk";
 import { registerReducer } from "./RegisteRedux/RegisterReducer";
+import { loginReducer } from "./LoginRedux/LoginReducer";
+
 const rootReducers= combineReducers({
     registerReducer,
+   loginReducer
 
 });
 
